a281f0e609e6cf94a3e65c07f89d5495a552139b
Massimo Brunno <56254825+mbrunno@users.noreply.github.com>
2025-04-07 15:53:58 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-// Homepage
+export default function Home() {
+  return (
+    <div>
+      <h1>Portale Agevolazioni Auto Elettriche</h1>
+      <p>Benvenuto nel portale delle agevolazioni!</p>
+    </div>
+  );
+}
